3ac80f822767395cb7eb382189adbf68a310d87b
MIGUEL <msabl005@fiu.edu>
2022-10-24 23:36:02 -0400

    Added Discover Tab: 2 rows, JSON parser, details view

---
+++ Sparkdev-Project/View/Cells/CurrentlyAiringShowCell.swift
@@ -0,0 +1,15 @@
+//
+//  AnimeGridCell.swift
+//  Sparkdev-Project
+//
+//  Created by Miguel Sablan on 10/23/22.
+//
+
+import UIKit
+
+class CurrentlyAiringShowCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+}
---
+++ Sparkdev-Project/View/Cells/UpcomingShowCell.swift
@@ -0,0 +1,14 @@
+//
+//  SecondGridCell.swift
+//  Sparkdev-Project
+//
+//  Created by Miguel Sablan on 10/24/22.
+//
+
+import UIKit
+
+class UpcomingShowCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+}
