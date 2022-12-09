1d83a1a5c4d8b2e459e5b74103f7dd8b01fc45b9
MIGUEL <msabl005@fiu.edu>
2022-12-09 18:26:22 -0500

    Added URLSession to improve loading, added refresh control, updated News UI

--- Sparkdev-Project/View/Cells/NewsCell.swift
+++ Sparkdev-Project/View/Cells/NewsCell.swift
@@ -11,9 +11,8 @@ class NewsCell: UITableViewCell {
 
     @IBOutlet weak var iconImageView: UIImageView!
     @IBOutlet weak var date: UILabel!
-    
     @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var previewLabel: UILabel!
+    @IBOutlet weak var newsBodyView: UIView!
     
-    
-
 }
